24ab99e816ab2b3b2bfc089dbf02e0f7b87db587
Rizqy H <rizqy22@gmail.com>
2018-11-18 10:11:15 +0700

    Adding style-resources-loader in order to use sass variables globally

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, 'src/styles/_variables.scss')
+      ]
+    }
+  }
+}
