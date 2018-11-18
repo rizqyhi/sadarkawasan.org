53d4840dda73c06b47625e71d3d9495359c1ed17
Rizqy H <rizqy22@gmail.com>
2018-11-18 11:11:13 +0700

    Base map implementation for conservation area view

--- src/views/ConservationArea.vue
+++ src/views/ConservationArea.vue
@@ -1,12 +1,53 @@
 <template>
   <div class="conservation-area-view">
-    Kawasan konservasi
+    <div class="map-wrapper" ref="caMap">
+      <l-map
+        :zoom="map.zoom"
+        :center="map.center"
+        :options="map.options"
+        style="height: 100%">
+        <v-tilelayer-googlemutant apikey="AIzaSyAZ9zikt49fhL3mdBLzU6iFaPXw6G8tV8M" />
+      </l-map>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import L from 'leaflet'
+import { LMap } from 'vue2-leaflet'
+import Vue2LeafletGoogleMutant from 'vue2-leaflet-googlemutant'
 
+export default {
+  components: {
+    LMap,
+    'v-tilelayer-googlemutant': Vue2LeafletGoogleMutant
+  },
+
+  data () {
+    return {
+      map: {
+        zoom: 5,
+        center: L.latLng(-2.5, 118),
+        options: {
+          zoomSnap: 0.5
+        }
+      }
+    }
+  },
+
+  methods: {
+    resizeMap () {
+      this.$refs.caMap.style.height = window.innerHeight + 'px'
+
+      window.addEventListener('resize', () => {
+        this.$refs.caMap.style.height = window.innerHeight + 'px'
+      })
+    }
+  },
+
+  mounted () {
+    this.resizeMap()
+  }
 }
 </script>
 
