9df4d8171640a7b80f3b060e30a7057516dff81a
Rizqy H <rizqy22@gmail.com>
2018-11-18 18:30:28 +0700

    Create and register page component

---
+++ src/components/Page.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="page">
+    <div class="page__wrapper">
+      <div class="page__header">
+        <slot name="header" />
+      </div>
+      <div class="page__body">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '~bootstrap/scss/mixins';
+@import '~bootstrap/scss/card';
+
+.page {
+  padding: 2rem;
+}
+.page__wrapper {
+  @extend .card;
+  border: none;
+  box-shadow: 5px 5px 20px 5px rgba($green, .1)
+}
+.page__header {
+  @extend .card-header;
+  padding: 2rem;
+  margin: 0;
+  font-size: 1.5rem;
+  color: $green;
+  background: rgba($green, .05);
+  border: none;
+
+  span[class^="ti-"] {
+    margin-right: .5rem
+  }
+}
+.page__body {
+  @extend .card-body;
+  padding: 2rem
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import Page from '@/components/Page'
 
 import './styles/app.scss'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+Vue.component('page', Page)
 
 new Vue({
   router,
