194974d4ab34505b2126ee590cc895b71bc57686
Rizqy H <rizqy22@gmail.com>
2018-11-18 19:51:40 +0700

    Create base page for campaign

--- src/views/Campaign.vue
+++ src/views/Campaign.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="campaign-view">
-    Kampanye sadar kawasan
-  </div>
+  <page class="campaign-view">
+    <template slot="header">
+      <span class="ti-gallery"></span>
+      Media & Kampanye
+    </template>
+  </page>
 </template>
 
 <script>
