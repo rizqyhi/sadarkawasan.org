f554e2022da81d4fe60691c138be6ee3bf69c0e9
Rizqy H <rizqy22@gmail.com>
2018-11-18 10:08:00 +0700

    Using bootstrap with scss

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './styles/app.scss'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
