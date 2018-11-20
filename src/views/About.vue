559b6f137d33089121a785ef52d24db11613f1a3
Rizqy H <rizqy22@gmail.com>
2018-11-20 22:32:01 +0700

    Add base writings for about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,36 @@
   <page class="about-view">
     <template slot="header">
       <span class="ti-heart"></span>
-      Tentang Sadarkawasan.org
+      Tentang #SadarKawasan
     </template>
+
+    <h5>#SadarKawasan</h5>
+    <hr>
+    <div class="row">
+      <div class="col-md-2">
+        <img src="https://pbs.twimg.com/profile_images/836488213099249664/ICOHUFSw_400x400.jpg" alt="" class="img-fluid">
+      </div>
+      <div class="col">
+        <p>Sadar kawasan merupakan media kampanye di sosial media untuk menghimbau masyarakat tidak mendatangi kawasan Cagar Alam untuk kepentingan pariwisata dan mengembalikan Cagar Alam ke fungsi awalnya yang hanya boleh didatangi untuk kepentingan penelitian.</p>
+        <ul>
+          <li>Instagram: <a href="https://www.instagram.com/sadarkawasan" target="_blank">@sadarkawasan</a></li>
+          <li>Blog: <a href="https://sadarkawasan.wordpress.com" target="_blank">sadarkawasan.wordpress.com</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <h5 class="mt-5">SadarKawasan.org</h5>
+    <hr>
+    <p>Situs ini dibangun sebagai media pelengkap kampanye #SadarKawasan, sekaligus sebagai referensi data status kawasan konservasi di Indonesia. Harapan kami, dengan situs ini masyarakat dapat mengetahui status dari suatu kawasan sebelum mengunjungi kawasan tersebut. Dan pada akhirnya turut berkontribusi dalam menjaga keseimbangan ekosistem kawasan konservasi di Indonesia.</p>
+    <p>Kode sumber dan data pada situs ini bersifat terbuka. Siapa saja bisa berkontribusi untuk turut mengembangkan atau memperkaya data kawasan. Data yang dimasukkan tentu saja harus berasal dari sumber yang valid.</p>
+    <ul>
+      <li>Respositori GitHub: <a href="https://github.com/rizqyhi/sadarkawasan.org">rizqyhi/sadarkawasan.org</a></li>
+      <li><a href="#" v-b-modal.license-modal>Lisensi software <em>opensource</em></a></li>
+    </ul>
+
+    <b-modal id="license-modal" title="Lisensi opensource" :hide-footer="true">
+      hello world
+    </b-modal>
   </page>
 </template>
 
