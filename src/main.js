1db7ea68b164a8a6b6dd3915e7986b4e4e30305d
Rizqy H <rizqy22@gmail.com>
2018-11-28 12:31:16 +0700

    Create loading component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import Page from '@/components/Page'
+import Loading from '@/components/Loading'
 
 import './styles/app.scss'
 
@@ -12,6 +13,7 @@ Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 Vue.component('page', Page)
+Vue.component('loading', Loading)
 
 new Vue({
   router,
