d45eace8df2977688ea51bd01b6b67c17a11972f
Rizqy H <rizqy22@gmail.com>
2018-11-28 12:30:41 +0700

    Add base configuration for Firebase

---
+++ src/utils/Firebase.js
@@ -0,0 +1,35 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
+}
+
+firebase.initializeApp(config)
+
+// firebase utils
+const db = firebase.firestore()
+// const auth = firebase.auth()
+// const currentUser = auth.currentUser
+
+// date issue fix according to firebase
+const settings = {
+  timestampsInSnapshots: true
+}
+db.settings(settings)
+
+// firebase collections
+const kawasanCollection = db.collection('kawasan')
+
+export {
+  firebase,
+  db,
+  // auth,
+  // currentUser,
+  kawasanCollection
+}
