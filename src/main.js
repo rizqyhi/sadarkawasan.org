3bc714e8094af080919c0d2ea7118c48e9f00e1b
Rizqy H <rizqy22@gmail.com>
2018-11-29 09:58:22 +0700

    Add Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -10,6 +11,16 @@ import Loading from '@/components/Loading'
 import './styles/app.scss'
 
 Vue.use(BootstrapVue)
+Vue.use(VueAnalytics, {
+  id: 'UA-100268600-1',
+  router,
+  autoTracking: {
+    screenview: true
+  },
+  debug: {
+    sendHitTask: process.env.NODE_ENV === 'production'
+  }
+})
 Vue.config.productionTip = false
 
 Vue.component('page', Page)
