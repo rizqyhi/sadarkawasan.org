0cf9d26e408b45986463ec0777d1cbffb8ed1ec4
Rizqy H <rizqy22@gmail.com>
2019-03-31 10:08:25 +0700

    Remove Algolia integration with Firestore

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,6 +1,5 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const algoliasearch = require('algoliasearch');
 const feedgram = require('./feedgram');
 
 // Initialize Firebase
@@ -9,46 +8,6 @@ const db = admin.firestore()
 db.settings({
   timestampsInSnapshots: true
 })
-
-// Initialize Algolia
-const ALGOLIA_ID = functions.config().algolia.app_id;
-const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
-const algoliaClient = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
-const KAWASAN_INDEX = 'dev_kawasan';
-
-/**
- * Save kawasan object to Algolia when created
- */
-exports.AlgoliaCreateKawasan = functions.firestore.document('kawasan/{kawasanId}')
-  .onCreate((snap, context) => {
-    let kawasan = snap.data()
-    kawasan.objectID = context.params.kawasanId
-
-    const index = algoliaClient.initIndex(KAWASAN_INDEX)
-    return index.addObject(kawasan)
-  })
-
-/**
- * Save kawasan object to Algolia after updated
- */
-exports.AlgoliaUpdateKawasan = functions.firestore.document('kawasan/{kawasanId}')
-  .onUpdate((snap, context) => {
-    let kawasan = snap.after.data()
-    kawasan.objectID = context.params.kawasanId
-
-    const index = algoliaClient.initIndex(KAWASAN_INDEX)
-    return index.saveObject(kawasan)
-  })
-
-/**
- * Delete kawasan object from Algolia when deleted
- */
-exports.AlgoliaDeleteKawasan = functions.firestore.document('kawasan/{kawasanId}')
-  .onDelete((snap, context) => {
-    const objectID = context.params.kawasanId
-    const index = algoliaClient.initIndex(KAWASAN_INDEX)
-    return index.deleteObject(objectID)
-  })
 
 /**
  * Fetch Instagram posts and save in Firestore
